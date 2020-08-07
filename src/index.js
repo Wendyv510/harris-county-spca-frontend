5267b53fb1b92336daf33bad31778bdbfe9508dc
Wendy Harris <wendyv510@hotmail.com>
2020-08-07 14:33:46 -0400

    imports in index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import {createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 

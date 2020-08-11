ff483d59be32d38b15a075eeb61f28ffeb8c5d10
Wendy Harris <wendyv510@hotmail.com>
2020-08-11 14:51:50 -0400

    navbar rework

--- src/containers/Navbar.js
+++ src/containers/Navbar.js
@@ -1,14 +1,16 @@
 import React from 'react' 
+import { Navlink } from 'react-router-dom' 
 
-const Navbar = () => {
-    return(
-        <div className="Navbar">
-            <Navlink to ="/" exact style={link}>Home</Navlink>
-            <Navlink to ="/cats" exact style={link}>Adoptable Cats</Navlink>
-            <Navlink to ="/dogs" exact style={link}>Adoptable Dogs</Navlink>
-            <Navlink to ="/apply" exact style={link}>Apply to Adopt</Navlink>
-        </div>
+const Navbar = () => (
+    <nav>
+        <ul>
+         <li><Navlink to ="/" exact style={link}>Home</Navlink></li>
+         <li><Navlink to ="/cats" exact style={link}>Adoptable Cats</Navlink></li>
+         <li><Navlink to ="/dogs" exact style={link}>Adoptable Dogs</Navlink></li>
+         <li><Navlink to ="/apply" exact style={link}>Apply to Adopt</Navlink></li>
+        </ul>
+    </nav>
     )
-}
+
 
 export default Navbar 

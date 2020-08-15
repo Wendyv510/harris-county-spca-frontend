fb99d7ce7dc2d87b5dc791e46db6f2390a8f7ce4
Wendy Harris <wendyv510@hotmail.com>
2020-08-15 11:51:24 -0400

    troubleshooting combine reducer

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ const rootReducer = combineReducers({
         dogReducer
 })
 
-let store = createStore(applicantReducer, composeEnhancers(applyMiddleware(thunk)))
+let store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <React.StrictMode>

b5ea4de2aa9fd7763098a8928637d2ee133fa86b
Wendy Harris <wendyv510@hotmail.com>
2020-11-03 14:38:19 -0500

    homepage additions

--- src/components/Applicants.js
+++ src/components/Applicants.js
@@ -3,8 +3,10 @@ import React from 'react'
 const Applicants = (props) => {
     
     return(
+        <div class="row row-cols-2  align-items-start">
         <div className="Applicants">
             {props.people.map(person => <li key={person.id}> Name:{person.attributes.name}<br></br> Pet Name:{person.attributes.pet_name}<br></br> Email:{person.attributes.email_address}<br></br> Phone Number:{person.attributes.phone_number}<br></br> Address:{person.attributes.address}</li>)}
+        </div>
         </div>
     )
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,13 +3,41 @@ import React from 'react'
 
 const Home = () => {
     return( 
-        <div className="Home" >
+        <div container="center">
+        <div className="Home">
             <h1> Harris County SPCA</h1>
             <div
             style={{
-            backgroundImage: `url("https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSPo-6LGECifMQPsnbAdETY6AQN_FvtgQLPcA&usqp=CAU")`,backgroundRepeat: 'no-repeat',width:'1050px',height:'1050px',color:'white'
+            backgroundImage: `url("https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSPo-6LGECifMQPsnbAdETY6AQN_FvtgQLPcA&usqp=CAU")`,backgroundRepeat: 'no-repeat',width:'526px',height:'226px',color:'white'
             }}>
             </div>
+            <div>
+                <h2>Mission Statement</h2>
+                <p>The mission of the SPCA of Harris County, located in Elmira, NY, is to protect companion animals. We are a no-kill shelter dedicated to preventing animal cruelty and overpopulation. We promote responsible pet stewardship by providing education, counseling and training to nurture and enhance the human-animal bond.
+                    Each year we take in and find happy new homes here in our community for close to 2000 animals. The SPCA staff, along with over 250 annual volunteers, rehabilitate, train, and care for these animals keeping them safe, happy, and socialized until they find their new forever homes.</p>
+            </div>
+            <div>
+                <h3>Hours of Operation</h3>
+                <ol>
+                    <li>Monday     11:00AM-5:00PM</li><br></br>
+                    <li>Tuesday    11:00AM-5:00PM</li><br></br>
+                    <li>Wendnesday 11:00AM-5:00PM</li><br></br>
+                    <li>Thursday   11:00AM-5:00PM</li><br></br>
+                    <li>Friday     11:00AM-5:00PM</li><br></br>
+                    <li>Saturday   11:00AM-2:00PM</li><br></br>
+                    <li>Sunday     Closed</li>
+                </ol>
+                <h3>Contact Info</h3>
+                <ol>
+                    <li>Address: 8984 Cty Rte.139
+                        Elmira, NY 14901 
+                    </li><br></br>
+                    <li>Phone:(607)734-9823</li><br></br>
+                    <li>Email:ChemungCountySPCA@gmail.com</li>
+                </ol>
+            </div>
+
+        </div>
         </div>
     )
 }
